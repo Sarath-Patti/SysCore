e87c462503429a0ae872d29a7b5d5e0f589d699b
Sarath Patti <sarathpatti@Saraths-MacBook-Air.local>
2026-07-17 15:23:25 +0530

    Fix C17 logging portability and update project version

--- include/common/logging.h
+++ include/common/logging.h
@@ -4,10 +4,10 @@
 #include "common/config.h"
 
 typedef enum {
-    SYSCORE_LOG_DEBUG = 0,
-    SYSCORE_LOG_INFO,
-    SYSCORE_LOG_WARN,
-    SYSCORE_LOG_ERROR
+  SYSCORE_LOG_DEBUG = 0,
+  SYSCORE_LOG_INFO,
+  SYSCORE_LOG_WARN,
+  SYSCORE_LOG_ERROR
 } syscore_log_level_t;
 
 /**
@@ -16,20 +16,28 @@ typedef enum {
 void syscore_log_init(syscore_log_level_t min_level);
 
 /**
- * Writes a log message to the appropriate stream (stdout/stderr) with formatting.
- * Typically invoked through the helper macros.
+ * Writes a log message to the appropriate stream (stdout/stderr) with
+ * formatting. Typically invoked through the helper macros.
  */
-void syscore_log_write(syscore_log_level_t level, const char* file, int line, const char* format, ...);
+void syscore_log_write(syscore_log_level_t level, const char *file, int line,
+                       const char *format, ...);
 
 /* Public logging macros */
-#define SYSCORE_LOG_INFO(fmt, ...)  syscore_log_write(SYSCORE_LOG_INFO, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define SYSCORE_LOG_WARN(fmt, ...)  syscore_log_write(SYSCORE_LOG_WARN, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define SYSCORE_LOG_ERROR(fmt, ...) syscore_log_write(SYSCORE_LOG_ERROR, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define SYSCORE_LOG_INFO(...)                                                  \
+  syscore_log_write(SYSCORE_LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+
+#define SYSCORE_LOG_WARN(...)                                                  \
+  syscore_log_write(SYSCORE_LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
+
+#define SYSCORE_LOG_ERROR(...)                                                 \
+  syscore_log_write(SYSCORE_LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 
 #if SYSCORE_LOG_ENABLE_DEBUG
-#define SYSCORE_LOG_DEBUG(fmt, ...) syscore_log_write(SYSCORE_LOG_DEBUG, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define SYSCORE_LOG_DEBUG(...)                                                 \
+  syscore_log_write(SYSCORE_LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #else
-#define SYSCORE_LOG_DEBUG(fmt, ...) do { (void)(fmt); } while (0)
+#define SYSCORE_LOG_DEBUG(...)                                                 \
+  do {                                                                         \
+  } while (0)
 #endif
-
 #endif // SYSCORE_COMMON_LOGGING_H
--- include/common/version.h
+++ include/common/version.h
@@ -4,11 +4,11 @@
 #define SYSCORE_VERSION_MAJOR 0
 #define SYSCORE_VERSION_MINOR 1
 #define SYSCORE_VERSION_PATCH 0
-#define SYSCORE_VERSION_STRING "0.1.0"
+#define SYSCORE_VERSION_STRING "0.4.0"
 
 /**
  * Returns the version string of the SysCore library.
  */
-const char* syscore_get_version_string(void);
+const char *syscore_get_version_string(void);
 
 #endif // SYSCORE_COMMON_VERSION_H
